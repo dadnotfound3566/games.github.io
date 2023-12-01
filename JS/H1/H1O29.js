31a3329078127f9ae2fb96cbc8a511cb46122ba4
dadnotfound3566 <145103699+dadnotfound3566@users.noreply.github.com>
2023-12-01 10:18:36 +0100

    (^*&%^$

--- JS/H1/H1O29.js
+++ JS/H1/H1O29.js
@@ -1,7 +1,7 @@
 var aantal = 5;
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(1000,200);
   canvas.parent('processing');
   frameRate(10);
   colorMode(RGB, 255, 255, 255, 1);
@@ -13,27 +13,28 @@ function setup() {
 function draw() {
   background('lavender');
   fill('black');
-  text("aantal = " + aantal,10,20);    
-  translate(225,225);
-  fill(178, 34, 34,0.7);
-  for (var n = 0;n < aantal;n++) {
-    ellipse(0,0,400,50);
-    rotate(360 / aantal);
-  }
-  fill(255, 195, 0,0.5);
-  for (var n = 0;n < aantal;n++) {
-    rect(0,0,75,75)
-    rotate(360 / aantal);
+  text("aantal = " + aantal,10,20);
+  translate(100,100);
+  for(n=0;n<6;n++){
+  tekenBloem();
+  translate(160,0)
   }
   if (keyIsDown(LEFT_ARROW) && aantal >= 2) {
     aantal--;
   }
   if (keyIsDown(RIGHT_ARROW)) {
     aantal++;
-  }   
-
-
+  }
 }
-
-
-
+function tekenBloem(){
+   fill(178, 34, 34,0.7);
+ for (var n = 0;n < aantal;n++) {
+    ellipse(0,0,160,20);
+    rotate(360 / aantal);
+  }
+  fill(255, 195, 0,0.5);
+  for (var n = 0;n < aantal;n++) {
+    rect(0,0,30,30)
+    rotate(360 / aantal);
+  }
+}
--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -7,10 +7,14 @@ function setup() {
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      fill(kleur);
-      rect(kolom,rij,50,50);
-    }
-  }
+for (var rij = 0; rij < 450; rij += 50) {
+for (var kolom = 0; kolom < 450; kolom += 50) {
+if ((rij + kolom) % 100 === 0) {
+fill('kleur');
+} else {
+fill('black');
+}
+rect(kolom, rij, 50, 50);
+}
+}
 }
