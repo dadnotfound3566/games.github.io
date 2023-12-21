84ffec511635e362343d0c71599078c9f04cc270
dadnotfound3566 <145103699+dadnotfound3566@users.noreply.github.com>
2023-12-21 12:55:25 +0100

    Update H2O08.js

--- JS/H2/H2O08.js
+++ JS/H2/H2O08.js
@@ -1,9 +1,11 @@
-var huisNummers = new Array(23,25,27,29,31,33);
-var huisEigenaren = new Array("Smiers","Dekker","Den Hartog","Tolboom","Kremer","Velthuizen");
-var kleur = 'lightgray';
+var huisNummers = new Array(23, 25, 27, 29, 31, 33);
+var huisEigenaren = new Array("Smiers", "Dekker", "Den Hartog", "Tolboom", "Kremer", "Velthuizen");
+var kleur1 = 'lightgray';
+var kleur2 = 'darkgrey';
+var huidigeKleur = kleur1;
 
 function setup() {
-  canvas = createCanvas(1000,300);
+  canvas = createCanvas(1000, 300);
   canvas.parent('processing');
   background('cornflowerblue');
   textFont("Georgia");
@@ -13,31 +15,39 @@ function setup() {
 
 function draw() {
   tekenAchtergrond();
-
-  
-    tekenHuis(kleur,13);
-    translate(150,0);
-
+  for (var i = 0; i < huisNummers.length; i++) {
+    var xPos = 50 + i * 150; 
+    tekenHuis(huidigeKleur, huisNummers[i], xPos, huisEigenaren[i]);
+    wisselKleur(); 
+  }
 }
 
-function tekenHuis(kleur,nr) {
+function tekenHuis(kleur, nr, xPos, eigenaar) {
   push();
+  translate(xPos, 0); 
   fill(kleur);
-  rect(0,110,150,150);
+  rect(0, 110, 150, 150);
   fill('gray');
-  triangle(0,110,150,110,75,20);
-  rect(20,170,50,90);
+  triangle(0, 110, 150, 110, 75, 20);
+  rect(20, 170, 50, 90);
   fill('indianred');
   textSize(30);
-  text(nr,75,190);
+  text(nr, 75, 190);
   fill('black');
   textSize(15);
-  text("naam",20,165);
+  text(eigenaar, 20, 165);
   pop();
 }
 
 function tekenAchtergrond() {
   fill('wheat');
-  rect(0,220,width,height - 220);
-  translate(50,0);
+  rect(0, 220, width, height - 220);
+}
+
+function wisselKleur() {
+  if (huidigeKleur === kleur1) {
+    huidigeKleur = kleur2;
+  } else {
+    huidigeKleur = kleur1;
+  }
 }
