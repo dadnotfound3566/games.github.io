e6c87876dede6eb31859e64b606f74657181e495
dadnotfound3566 <145103699+dadnotfound3566@users.noreply.github.com>
2023-12-23 23:20:58 +0100

    Update H2O13.js

--- JS/H2/H2O13.js
+++ JS/H2/H2O13.js
@@ -12,38 +12,54 @@ var yJos = 300;
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
-  frame = loadImage("images/sprites/Jos100px/Jos_0.png");
+
+  for (var b = 0; b < aantalBeeldjes; b++) {
+  frame = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
+  animatie.push(frame)
+  }
+  
 }
+  
 
 function setup() {
   canvas = createCanvas(900,600);
   canvas.parent('processing');
-  frameRate(10);
+  frameRate(2);
   celGrootte = width / aantalKolommenRaster;
 }
 
+// animatie moet blijven tot er op een andere knop wordt geklikt
 function draw() {
   background(brug);
   tekenRaster();
 
-  if (keyIsDown(LEFT_ARROW)) {
+  if (keyIsDown(LEFT_ARROW)) { 
     xJos -= celGrootte;
+     nummer = 2;
   }
+
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
+    nummer = 1;
   }
   if (keyIsDown(UP_ARROW)) {
-    yJos-=celGrootte;
+    yJos -= celGrootte;
+    nummer = 4
   }
   if (keyIsDown(DOWN_ARROW)) {
     yJos += celGrootte;
+    nummer = 5
   }
+
+  xJos = constrain(xJos, 0, width - celGrootte);
+  yJos = constrain(yJos, 0, height - celGrootte);
+
   
-  xJos = constrain(xJos,0,width - celGrootte);
-  yJos = constrain(yJos,0,height - celGrootte);
-  
-  image(frame,xJos,yJos);
+  image(animatie[nummer], xJos, yJos);
+  nummer = (frameCount) % aantalBeeldjes;
 }
+
+
 
 function tekenRaster() {
   push();
