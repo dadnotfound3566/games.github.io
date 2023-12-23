ff678269747cec51df67646448e3a2a2d6c6270c
dadnotfound3566 <145103699+dadnotfound3566@users.noreply.github.com>
2023-12-23 20:35:48 +0100

    Update H2O09.js

--- JS/H2/H2O09.js
+++ JS/H2/H2O09.js
@@ -1,29 +1,65 @@
 var foto;
 var pixelKleur;
-var rood,groen,blauw;
+var rood, groen, blauw;
+
+var aantalRijenRaster = 18;
+var aantalKolommenRaster = 18;
+var celGrootte;
 
 function preload() {
   foto = loadImage("images/brieck_klein.jpg");
 }
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(450, 450);
   canvas.parent('processing');
   textFont("Georgia");
   textSize(18);
   noStroke();
   foto.loadPixels();
+  celGrootte = width / aantalKolommenRaster;
+  image(foto, 0, 0, width, height);
+  tekenRaster();
 }
 
 function draw() {
-  background(foto);
-  pixelKleur = foto.get(mouseX,mouseY);
+  pixelKleur = foto.get(mouseX, mouseY);
   rood = pixelKleur[0];
   groen = pixelKleur[1];
   blauw = pixelKleur[2];
-  
-  fill(pixelKleur);
-  rect(0,410,450,40);
-  fill('white');  
-  text("RGB-waarden: rood = " + rood + " groen = " + groen+" blauw = " + blauw,10,435);
+}
+
+function tekenRaster() {
+  for (var rij = 0; rij < aantalRijenRaster; rij++) {
+    for (var kolom = 0; kolom < aantalKolommenRaster; kolom++) {
+      var x = kolom * celGrootte;
+      var y = rij * celGrootte;
+      var gemiddeldeKleur = gemiddeldePixel(x, y, celGrootte, celGrootte);
+      fill(gemiddeldeKleur);
+      rect(x, y, celGrootte, celGrootte);
+    }
+  }
+}
+
+function gemiddeldePixel(x, y, breedte, hoogte) {
+  var totaalRood = 0;
+  var totaalGroen = 0;
+  var totaalBlauw = 0;
+  var aantalPixels = 0;
+
+  for (var kolomPositie = x; kolomPositie < x + breedte; kolomPositie++) {
+    for (var rijPositie = y; rijPositie < y + hoogte; rijPositie++) {
+      var pixel = foto.get(kolomPositie, rijPositie);
+      totaalRood += pixel[0];
+      totaalGroen += pixel[1];
+      totaalBlauw += pixel[2];
+      aantalPixels++;
+    }
+  }
+
+  var gemiddeldeRood = totaalRood / aantalPixels;
+  var gemiddeldeGroen = totaalGroen / aantalPixels;
+  var gemiddeldeBlauw = totaalBlauw / aantalPixels;
+
+  return color(gemiddeldeRood, gemiddeldeGroen, gemiddeldeBlauw);
 }
